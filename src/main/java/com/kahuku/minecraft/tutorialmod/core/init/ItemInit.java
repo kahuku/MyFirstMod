c6b57cb6ed90ef1324c0964d3d026a5dbb65ec57
kahuku <buzzinbubba@gmail.com>
2023-06-24 14:57:58 -0600

    example entity spawn egg

--- src/main/java/com/kahuku/minecraft/tutorialmod/core/init/ItemInit.java
+++ src/main/java/com/kahuku/minecraft/tutorialmod/core/init/ItemInit.java
@@ -3,6 +3,7 @@ package com.kahuku.minecraft.tutorialmod.core.init;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,6 +17,9 @@ public final class ItemInit {
 
     public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<ForgeSpawnEggItem> EXAMPLE_ENTITY_SPAWN_EGG = ITEMS.register("example_entity_spawn_egg",
+            () -> new ForgeSpawnEggItem(EntityInit.EXAMPLE_ENTITY, 0x6d6e6e, 0x03fcfc, new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(16)));
 
     // Block items
     public static final RegistryObject<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block",
